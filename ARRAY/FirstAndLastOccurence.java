80eb9684389eac0523c6fb76d064efff8de4de23
Sachin kumar jha <sachinkumarjhasachinkumarjha9@gmail.com>
2025-09-05 00:26:41 +0530

    question added

---
+++ ARRAY/FirstAndLastOccurence.java
@@ -0,0 +1,52 @@
+package ARRAY;
+
+public class FirstAndLastOccurence {
+    public static int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        return new int[]{first, last};
+    }
+
+    private static int findFirst(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        int first = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                first = mid;
+                high = mid - 1; // keep searching left
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return first;
+    }
+
+    private static int findLast(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        int last = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                last = mid;
+                low = mid + 1; // keep searching right
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return last;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {5, 7, 7, 8, 8, 10};
+        int[] ans = searchRange(nums, 8);
+
+        for (int i : ans) {
+            System.out.print(i + " ");
+        }
+    }
+}
