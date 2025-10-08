a0d9e951b791bf71149f0283fe2fa6148b983859
Sachin kumar jha <sachinkumarjhasachinkumarjha9@gmail.com>
2025-10-08 23:25:35 +0530

    question added

--- ARRAY/RotateArrayBy90degree.java
+++ ARRAY/RotateArrayBy90degree.java
@@ -57,7 +57,7 @@ public static void reverseRows(int[][] matrix) {
     }
 
     public static void main(String[] args) {
-        int matrix[][]={{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}};
+        int matrix[][]={{1,2,3},{4,5,6},{7,8,9}};;
 
       for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix.length;j++){
