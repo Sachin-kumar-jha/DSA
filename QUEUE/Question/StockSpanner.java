c2bb0f9119b453ccdcb89e16487755e2eb3d9307
Sachin kumar jha <sachinkumarjhasachinkumarjha9@gmail.com>
2025-11-04 21:30:19 +0530

    question added

---
+++ QUEUE/Question/StockSpanner.java
@@ -0,0 +1,33 @@
+package QUEUE.Question;
+class StockSpanner {
+    private static class Node {
+        int price;
+        int span;
+        Node(int p, int s) { price = p; span = s; }
+    }
+
+    private Deque<Node> stack;
+
+    public StockSpanner() {
+        stack = new ArrayDeque<>();
+    }
+    
+    public int next(int price) {
+        int currSpan = 1;
+        // Pop while previous prices are <= current price and accumulate spans
+        while (!stack.isEmpty() && stack.peekLast().price <= price) {
+            currSpan += stack.peekLast().span;
+            stack.removeLast();
+        }
+        // Push current price with its span
+        stack.addLast(new Node(price, currSpan));
+        return currSpan;
+    }
+
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
