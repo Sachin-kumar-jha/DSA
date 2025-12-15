98793a4d017c7dd24284995891e6be35e4f449ca
Sachin kumar jha <sachinkumarjhasachinkumarjha9@gmail.com>
2025-12-15 12:32:39 +0530

    diameter calculating using Info class

--- Tree/Impl/HeightOfTree.java
+++ Tree/Impl/HeightOfTree.java
@@ -55,15 +55,40 @@ public class HeightOfTree {
      return lfcSum+rfcSum+root.data;
     }
 
-    public static int calculateDiameter(Node root){
+
+    static class Info{
+        int diam;
+        int ht;
+        Info(int diam,int ht){
+            this.diam=diam;
+            this.ht=ht;
+        }
+    }
+    /*APPROACH 1 */
+    public static int calculateDiameter1(Node root){
         if(root==null)return 0;
-        int ldiam=calculateDiameter(root.left);
-        int rdiam=calculateDiameter(root.right);
+        int ldiam=calculateDiameter1(root.left);
+        int rdiam=calculateDiameter1(root.right);
 
         int selfdiam=height(root.left)+height(root.right)+1;
 
         return Math.max(Math.max(ldiam,rdiam),selfdiam);
     }
+
+     /*APPROACH 2 */
+    public static Info calculateDiameter2(Node root){
+
+        if(root==null) return new Info(0, 0);
+        Info lftInfo=calculateDiameter2(root.left);
+        Info rftInfo=calculateDiameter2(root.right);
+         
+        int diam = Math.max(Math.max(lftInfo.diam, rftInfo.diam),lftInfo.ht+rftInfo.ht+1);
+        int ht=Math.max(lftInfo.ht, rftInfo.ht)+1;
+
+return new Info(diam, ht);
+    }
+
+
     public static void main(String[] args) {
   int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
    Node tree=buildTree(nodes);
@@ -71,7 +96,7 @@ public class HeightOfTree {
 // //  System.out.print(height(tree));
 //  System.out.println(calculateNodes(tree));
 
-System.out.println(calculateDiameter(tree));
+System.out.println(calculateDiameter2(tree).diam);
 
 //  System.out.println(sumOfNodes(tree));
     }
